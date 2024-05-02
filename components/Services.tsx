c8e3a782145392e9555cebc2a3072c82da00e705
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 22:10:46 +0300

    creating the Services component

---
+++ components/Services.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="bg-[#121121] pt-[4rem] md:pt-[8rem] pb-[5rem]">
+      <p className="heading">
+        My <span className="text-yellow-400">Services</span>
+      </p>
+    </div>
+  );
+};
+
+export default Services;
