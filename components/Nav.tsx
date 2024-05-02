68462c55013d84995876a454df91c937782fb833
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 17:49:13 +0300

    updating the nav component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,8 +9,8 @@ const Nav = ({ onOpenNav }: Props) => {
     <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
       <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
         <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
-          WEB
-          <span className="text-yellow-300">DEV</span>
+          Tsegaye
+          <span className="text-yellow-300">Talegngeta</span>
         </h1>
         <div className="nav-link">Home</div>
         <div className="nav-link">Services</div>
