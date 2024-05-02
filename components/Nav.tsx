d798fb3cbab89ae3d7def9b501a48cbd3d993884
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 20:54:25 +0300

    Updating MobileNav and Nav components

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,7 +11,7 @@ const Nav = ({ onOpenNav }: Props) => {
       <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
         <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
           Tsegaye
-          <span className="text-yellow-300">Talegngeta</span>
+          <span className="text-yellow-300">Talegn</span>
         </h1>
         <div className="nav-link">Home</div>
         <div className="nav-link">Services</div>
