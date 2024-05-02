dfd47451322eba76b34c29195f3915924c4aaa15
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 17:48:57 +0300

    creating the hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-[88vh] bg-slate-700 bg-cover bg-center">
+      <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+        <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+          Hello, I am <span className="text-yellow-400">Tsegaye!</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
