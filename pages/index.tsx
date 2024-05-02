b61a595dc2fa96c77a7c00c199c33ed8e8b288ba
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 10:49:11 +0300

    created a boolean state nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
+import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
-import React from "react";
+import React, { useState } from "react";
 
 const HomePage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
   return (
     <div className="overflow-x-hidden">
       <div>
-        <Nav />
+        <MobileNav nav={nav} onCloseNav={closeNav} />
+        <Nav onOpenNav={openNav} />
       </div>
     </div>
   );
