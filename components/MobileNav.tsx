2bc70fe2d5f927a55bd863856da60e82bcf24b2a
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 10:48:21 +0300

    included conditional rendering for the mobile nav

---
+++ components/MobileNav.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface Props {
+  nav: boolean;
+  onCloseNav: () => void;
+}
+
+const MobileNav = ({ nav, onCloseNav }: Props) => {
+  const navAnimation = nav ? "translate-x-0" : "translate-x-[-100%]";
+
+  return (
+    <div
+      className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}
+    >
+      <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+        <div className="nav-link-mobile">Home</div>
+        <div className="nav-link-mobile">Services</div>
+        <div className="nav-link-mobile">About</div>
+        <div className="nav-link-mobile">Project</div>
+        <div className="nav-link-mobile">Contact</div>
+      </div>
+      <div
+        onClick={onCloseNav}
+        className="absolute cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-400"
+      >
+        <div>X</div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
