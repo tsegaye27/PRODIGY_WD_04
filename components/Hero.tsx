3e70002ac6b4c64b07b04723136fd0088a43caeb
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 19:32:32 +0300

    styling the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,42 @@
+import { ArrowDownCircleIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
   return (
     <div className="h-[88vh] bg-slate-700 bg-cover bg-center">
       <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
-        <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-          Hey, I am <span className="text-yellow-400">Tsegaye!</span>
-        </h1>
+        <div>
+          <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+            Hey, I am <span className="text-yellow-400">Tsegaye!</span>
+          </h1>
+          <h2 className="text-[2rem] text-[#55e6a5] font-bold uppercase mt-[1rem]">
+            Web Developer
+          </h2>
+          <p className="text-[#ffffff92] text-[18px] mt-[1.5rem]">
+            I am a web developer with over 3 years of experience. I specialize
+            in creating websites and web applications that are fast, responsive,
+            and user-friendly. I am passionate about using the latest
+            technologies to build high-quality websites that help businesses
+            grow.
+          </p>
+          <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+            <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+              <p>Download CV</p>
+              <ArrowDownCircleIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+            </button>
+          </div>
+        </div>
+        <div className="w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]">
+          <Image
+            src="/images/u1.jpg"
+            priority
+            alt="user"
+            layout="fill"
+            sizes="500px"
+            className="object-cover rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
