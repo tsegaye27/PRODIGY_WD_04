1a32a964d7430e8942b832afe97fb8209d91602d
tsegaye27 <ttalegn29@gmail.com>
2024-05-03 00:15:44 +0300

    styling services component

--- components/Services.tsx
+++ components/Services.tsx
@@ -9,7 +9,7 @@ const Services = () => {
   return (
     <div className="bg-[#121121] pt-[4rem] md:pt-[8rem] pb-[5rem]">
       <p className="heading">
-        My <span className="text-yellow-400">Services</span>
+        My <span className="text-primary-400">Services</span>
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white">
         <div>
