ab353aaa2b2b9bf26003b8476684443be57ee4e3
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 09:46:52 +0300

    creating navigation links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+      <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+        <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+          WEB
+          <span className="text-yellow-300">DEV</span>
+        </h1>
+        <div className="nav-link">Home</div>
+        <div className="nav-link">Services</div>
+        <div className="nav-link">About</div>
+        <div className="nav-link">Project</div>
+        <div className="nav-link">Contact</div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
