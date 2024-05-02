2bfdb25d96e313ab3f2deb9c8cfa7899c5114799
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 22:42:12 +0300

    creating the skills components

---
+++ components/SkillList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SkillList = () => {
+  return <div>SkillList</div>;
+};
+
+export default SkillList;
---
+++ components/Skills.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
+      <h1 className="heading">
+        Education & <span className="text-yellow-400">Skills</span>
+      </h1>
+      <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center"></div>
+    </div>
+  );
+};
+
+export default Skills;
