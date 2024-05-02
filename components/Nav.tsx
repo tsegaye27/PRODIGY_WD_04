38d57b11f17080048a1566a3b43fb73581ad4263
tsegaye27 <ttalegn29@gmail.com>
2024-05-02 10:48:36 +0300

    included conditional rendering

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const Nav = () => {
+interface Props {
+  onOpenNav: () => void;
+}
+
+const Nav = ({ onOpenNav }: Props) => {
   return (
     <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
       <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
@@ -13,6 +17,12 @@ const Nav = () => {
         <div className="nav-link">About</div>
         <div className="nav-link">Project</div>
         <div className="nav-link">Contact</div>
+        <div onClick={onOpenNav}>
+          {/* <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300"></Bars3Icon> */}
+          <div className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300">
+            =
+          </div>
+        </div>
       </div>
     </div>
   );
