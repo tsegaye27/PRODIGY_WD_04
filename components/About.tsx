1fdb43f48bcf7c6b05adc884a8eb6b2e3191fdba
tsegaye27 <ttalegn29@gmail.com>
2024-05-05 00:07:47 +0300

    including links

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ArrowDownCircleIcon, ArrowDownIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 import React from "react";
@@ -16,15 +17,41 @@ const About = () => {
           <div className="mb-[3rem] flex items-center md:space-x-10">
             <span className="w-[100px] hidden md:block h-[5px] bg-slate-400 rounded-sm"></span>
             <p className="text-[19px] text-slate-300">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-              obcaecati cumque repellat facere quisquam molestiae? Harum
-              voluptates quidem perspiciatis vel, cumque totam
+              I am Tsegaye Talegngeta, a 4th-year software engineering student
+              at Bahirdar Institute of Technology. As a web developer, I am
+              passionate about creating robust and dynamic web applications.
+              With a focus on continuous learning, I stay up-to-date with the
+              latest trends and technologies in the field. I have a keen eye for
+              design and user experience, and I believe in the power of
+              well-crafted interfaces. Let us connect and collaborate on
+              exciting projects.
             </p>
           </div>
-          <button className="px-[2rem] hover:bg-primary-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-secondary-500 text-black flex items-center space-x-2">
-            <p>Download CV</p>
-            <ArrowDownIcon className="w-[1.6rem] h-[1.7rem] text-black" />
-          </button>
+          <div className="flex justify-center gap-[2rem]">
+            <a href="https://github.com/tsegaye27" target="_blank">
+              <Image
+                src="/icons/github.svg"
+                alt="github"
+                priority
+                width={50}
+                height={50}
+                className="w-[40px] h-[40px] text-white bg-primary-400 rounded-full p-[0.5rem] cursor-pointer"
+              />
+            </a>
+            <a
+              href="https://linkedin.com/in/tsegaye-talegngeta27"
+              target="_blank"
+            >
+              <Image
+                src="/icons/linkedin.svg"
+                alt="github"
+                priority
+                width={50}
+                height={50}
+                className="w-[40px] h-[40px] text-white bg-primary-400 rounded-full p-[0.5rem] cursor-pointer"
+              />
+            </a>
+          </div>
         </div>
         <div className="lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative">
           <Image
