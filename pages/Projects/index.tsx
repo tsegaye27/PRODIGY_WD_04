44bf24d4600ff1baf6e1b2ac0ef3f2edf7ff2f76
tsegaye27 <ttalegn29@gmail.com>
2024-05-30 14:20:50 +0300

    refactoring

---
+++ pages/Projects/index.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div
+      id="projects"
+      className="bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]"
+    >
+      <h1 className="heading">
+        Pro<span className="text-primary-400">Jects</span>
+      </h1>
+      <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
+        <div>
+          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-300 relative w-[100%] h-[200px] md:h-[300px]">
+            <Image
+              src="/images/p1.png"
+              alt="portfolio"
+              layout="fill"
+              className="object-contain"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-300 relative w-[100%] h-[200px] md:h-[300px]">
+            <Image
+              src="/images/p2.png"
+              alt="portfolio"
+              layout="fill"
+              className="object-contain"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-300 relative w-[100%] h-[200px] md:h-[300px]">
+            <Image
+              src="/images/p3.png"
+              alt="portfolio"
+              layout="fill"
+              className="object-contain"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ pages/Services/index.tsx
@@ -0,0 +1,60 @@
+import {
+  CodeBracketSquareIcon,
+  CommandLineIcon,
+  RocketLaunchIcon,
+} from "@heroicons/react/20/solid";
+import React from "react";
+
+const Services = () => {
+  return (
+    <div
+      id="services"
+      className="bg-[#121121] pt-[4rem] md:pt-[8rem] pb-[5rem]"
+    >
+      <p className="heading">
+        My <span className="text-primary-400">Services</span>
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white">
+        <div>
+          <div className="bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
+            <CodeBracketSquareIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+              Frontend
+            </h1>
+            <p className="text-[15px] text-[#d3d2d2] font-normal">
+              Offering custom frontend solutions, specializing in React, Vue.js,
+              and Angular, to elevate online presence with captivating user
+              experiences.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="bg-orange-700 hover:scale-110 transform transition-all duration-300 uppercase font-semibold text-center p-[2rem]">
+            <RocketLaunchIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+              Backend
+            </h1>
+            <p className="text-[15px] text-[#d3d2d2] font-normal">
+              Providing backend solutions with Node.js, Express, and MongoDB to
+              create scalable and efficient web applications.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="bg-blue-700 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]">
+            <CommandLineIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+              FullStack
+            </h1>
+            <p className="text-[15px] text-[#d3d2d2] font-normal">
+              Offering full-stack development services with MERN stack to create
+              dynamic and interactive web applications.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
