16a94ec528ec81e3d0698da442e38fe0bca8be52
tsegaye27 <ttalegn29@gmail.com>
2024-05-30 14:18:42 +0300

    refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,42 @@
-import About from "@/components/About";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import MobileNav from "@/components/MobileNav";
-import Nav from "@/components/Nav";
-import Projects from "@/components/Projects";
-import Services from "@/components/Services";
-import Skills from "@/components/Skills";
+import Head from "next/head";
+import About from "./About";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import Projects from "./Projects";
+import Services from "./Services";
+import Skills from "./Skills";
 import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+
+type Props = {
+  nav: boolean;
+  onCloseNav: () => void;
+};
+
+const MobileNav = ({ nav, onCloseNav }: Props) => {
+  const navAnimation = nav ? "translate-x-0" : "translate-x-[-100%]";
+
+  return (
+    <div
+      className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}
+    >
+      <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+        <div className="nav-link-mobile">Home</div>
+        <div className="nav-link-mobile">About</div>
+        <div className="nav-link-mobile">Services</div>
+        <div className="nav-link-mobile">Project</div>
+        <div className="nav-link-mobile">Contact</div>
+      </div>
+      <div
+        onClick={onCloseNav}
+        className="absolute z-[1000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-primary-400"
+      >
+        <XMarkIcon />
+      </div>
+    </div>
+  );
+};
 
 const HomePage = () => {
   const [nav, setNav] = useState(false);
@@ -14,28 +44,33 @@ const HomePage = () => {
   const closeNav = () => setNav(false);
 
   return (
-    <div className="overflow-x-hidden">
-      <div>
-        {/* Nav Section */}
-        <MobileNav nav={nav} onCloseNav={closeNav} />
-        <Nav onOpenNav={openNav} />
+    <>
+      <Head>
+        <title>TsegayeTalegn</title>
+      </Head>
+      <div className="overflow-x-hidden">
+        <div>
+          {/* Nav Section */}
+          <MobileNav nav={nav} onCloseNav={closeNav} />
+          <Nav onOpenNav={openNav} />
 
-        {/* Hero Section */}
-        <Hero />
-        <div className="relative z-[30]">
-          {/* About Section */}
-          <About />
-          {/* Services Section */}
-          <Services />
-          {/* Skills Section */}
-          <Skills />
-          {/* Projects Section */}
-          <Projects />
-          {/* Footer Section */}
-          <Footer />
+          {/* Hero Section */}
+          <Hero />
+          <div className="relative z-[30]">
+            {/* About Section */}
+            <About />
+            {/* Services Section */}
+            <Services />
+            {/* Skills Section */}
+            <Skills />
+            {/* Projects Section */}
+            <Projects />
+            {/* Footer Section */}
+            <Footer />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
